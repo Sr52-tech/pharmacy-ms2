6a7210ce40454fe1381cba6b874852154aefd327
Moaz Mostafa <moazmostafa@Moazs-MacBook-Pro.local>
2023-12-07 20:19:20 +0300

    products

---
+++ src/pages/CosmeticProducts.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react';
+import { ProductsContext } from '../global/ProductsContext';
+import { ProductCard } from '../components/ProductCard';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Nav from '../components/Navbar';
+import Footer from '../components/Footer';
+
+export const CosmeticProducts = () => {
+    const { products } = useContext(ProductsContext);
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const medicineProducts = products.filter(product => product.Category === 'Cosmetic');
+
+    return (
+        <>
+            <Nav />
+            <div style={{
+                display: 'grid',
+                gridTemplateColumns: isSmallScreen ? 'repeat(2, 1fr)' : 'repeat(4, 1fr)',
+                gap: '1rem',
+                textAlign: 'center',
+                margin: '2rem',
+            }}>
+                {medicineProducts.map((product) => (
+                    <ProductCard
+                        key={product.id}
+                        imageSrc={product.ProductImage}
+                        description={product.Description}
+                        price={product.ProductPrice}
+                    />
+                ))}
+            </div>
+            <Footer />
+        </>
+    );
+};
