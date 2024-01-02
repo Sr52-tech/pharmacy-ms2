5edc19c2f514a5022f0373da49508e0ffbde2f85
Shahad <prestigebeetle@gmail.com>
2024-01-02 19:18:43 +0300

    stylig shit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,20 +5,37 @@ import { auth } from '../firebase-config';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Avatar } from 'flowbite-react';
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { ToastContainer, toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
+import { addUser, removeUser, selectUserRole } from "../redux/pharmacySlice";
+
 
 function Nav({ user }) {
     const productData = useSelector((state) => state.pharmacy.productData);
     const userInfo = useSelector((state) => state.pharmacy.userInfo);
+    const dispatch = useDispatch()
 
     const navigate = useNavigate();
+
+    const handleSignout = () => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            toast.success('Signout successful')
+            dispatch(removeUser())
+        }).catch((error) => {
+            // An error happened.
+            console.log(error)
+        });
+    }
 
     console.log(userInfo)
 
     return (
         <Navbar fluid rounded>
-            <Navbar.Brand href="https://flowbite-react.com%22%3E/">
-                <img src="/logo192.png" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+            <Navbar.Brand href="/">
+                <img src="/logo.png" className="mr-3 h-6 sm:h-9" alt="Pharmacy Logo" />
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Pharmacy</span>
             </Navbar.Brand>
             <div className="flex md:order-2 items-center">
                 <Navbar.Toggle />
@@ -26,24 +43,45 @@ function Nav({ user }) {
                     <ShoppingCartIcon count={productData.length} />
                 </Link>
                 <Link to="login" className="flex md:order-2 items-center">
-                    {userInfo && <Avatar img={userInfo.image || 'default-image-url'} alt="avatar" rounded />}
+                    {userInfo && userInfo.image && <Avatar img={userInfo?.image || 'default-image-url'} alt="avatar" rounded />}
                     {userInfo && <p className="ml-2">{userInfo.name}</p>}
                 </Link>
-                {!userInfo && <Button onClick={() => navigate('/login')}>Login</Button>}
+                {!userInfo && <Button onClick={() => navigate('/login')}>Login</Button>}    
+                {!userInfo && <Button  className="ml-2"
+                onClick={() => navigate('/signup')}>Signup</Button>}
             </div>
+
             <Navbar.Collapse>
-                <Navbar.Link href="#" active>
+            <div >
+                <Navbar.Link href="/" active >
                     Home
                 </Navbar.Link>
+            </div>
                 <Navbar.Link href="#">About</Navbar.Link>
                 {userInfo && userInfo.Role === 'admin' && (
                     <Navbar.Link as={Link} to="/staffdashboard">
                         Admin Dashboard
                     </Navbar.Link>
                 )}
-                <Navbar.Link href="#">Contact</Navbar.Link>
-                <Navbar.Link as={Link} to="/products">Products</Navbar.Link> {/* Add this line */}
+                {userInfo && (
+                    <Navbar.Link onClick={handleSignout}>
+                        Sign Out
+                    </Navbar.Link>
+                )}
             </Navbar.Collapse>
+
+            <ToastContainer
+                position="top-left"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
         </Navbar>
     );
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -7,6 +7,7 @@ import { MedicineProducts } from "./MedicineProducts.jsx";
 import Carousel from "../components/Carousel.jsx";
 import Categories from "../components/Categories.jsx";
 import Testimonial from "../components/Testimonial.jsx";
+import Info from "../components/Info.jsx";
 
 function Main() {
     return (
@@ -18,7 +19,7 @@ function Main() {
             <div className="row flex flex-wrap justify-center">
                 <Categories
 
-                    imgSrc="/Pics/Cards/Designer.png"
+                    imgSrc="/Pics/Cards/vitamine.png"
                     title="Medicine Products"
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     link="/medicine"
@@ -27,7 +28,7 @@ function Main() {
 
                 <Categories
 
-                    imgSrc="/Pics/Cards/Allergy.png"
+                    imgSrc="/Pics/Cards/Cosmetic.png"
                     title="Cosmetic Products"
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     link="/cosmetic"
@@ -35,14 +36,26 @@ function Main() {
 
                 <Categories
 
-                    imgSrc="/Pics/SkinCare.png"
+                    imgSrc="/Pics/Cards/hygene.png"
                     title="Hygiene Products"
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     link="/hygiene"
                 />
             </div>
-
             <hr style={{ width: '70%', height: '1px', backgroundColor: 'black', marginTop: '100px', marginLeft: 'auto', marginRight: 'auto' }} />
+            <h1 style={{ marginLeft: '110px', marginTop: '60px' }} class="text-5xl font-extrabold dark:text-white m-4">Who<small class="ms-2 font-semibold text-gray-500 dark:text-gray-400">We Are</small></h1>
+
+            <div style={{ display: 'grid', gridTemplateColumns: 'auto auto', alignItems: 'center', height: '70%', width: '70%', marginLeft: '110px'}}>
+                <Info 
+                    imgSrc={"/Pics/info.png"}
+                />
+                <h3 style={{ marginLeft: '100px' }}>
+                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, 
+                <br></br>a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. 
+                <br></br>Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" The Extremes of Good and Evil by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</h3>
+            </div>
+
+            <hr style={{ width: '70%', height: '1px', backgroundColor: 'black', marginLeft: 'auto', marginRight: 'auto' }} />
             <h1 style={{ marginLeft: '115px', marginBottom: '50px', marginTop: '60px' }} class="text-5xl font-extrabold dark:text-white m-4">What<small class="ms-2 font-semibold text-gray-500 dark:text-gray-400">People Say</small></h1>
 
             <div className="row flex flex-wrap justify-center" style={{ marginBottom: '100px' }}>
